d5aa31dc6efebe1edecabe5ef74753eee344fe70
Jiawei <xujavacn@gmail.com>
2018-09-01 10:33:46 +0800

    init commit

---
+++ FTSDK/FTSDK.h
@@ -0,0 +1,19 @@
+//
+//  FTSDK.h
+//  FTSDK
+//
+//  Created by Glassesd on 2018/9/1.
+//  Copyright © 2018 Glassesd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FTSDK.
+FOUNDATION_EXPORT double FTSDKVersionNumber;
+
+//! Project version string for FTSDK.
+FOUNDATION_EXPORT const unsigned char FTSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FTSDK/PublicHeader.h>
+
+
